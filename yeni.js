815b10aea812b920e02c5b0f9b2790d3664f3377
Suray959 <mirzeyevasuray3@gmail.com>
2024-04-01 21:44:21 +0400

    yeni

--- yeni.js
+++ yeni.js
@@ -1,5 +1,4 @@
 
-
 let text = document.querySelector('.task');
 let add = document.querySelector('#addbtn');
 let myList = document.querySelector('.mylist');
@@ -8,40 +7,33 @@ let arr = [];
 add.addEventListener('click', function () {
     let b = text.value;
     arr.push(b);
-    let li = document.createElement('li');
-    li.textContent = b;
-
-    let edit = document.createElement('button');
-    edit.textContent = 'Edit';
-    edit.style.backgroundColor='red'
-    edit.style.color='white'
-    edit.style.margin='10px'
-
-    edit.addEventListener('click', function () {
-        let newText =prompt(b);
-        if (newText !== null) {
-            b = newText;
-            li.textContent = newText;
-        }
-    });
-
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.style.backgroundColor='gray'
-    deleteButton.style.color='black'
-    deleteButton.style.margin='10px'
-    deleteButton.addEventListener('click', function () {
-        arr.splice(arr.indexOf(b), 1);
-        myList.removeChild(li);
-    });
-
-    li.appendChild(deleteButton);
-    li.appendChild(edit);
-    myList.appendChild(li);
+    renderList();
 });
-
-
-
+function renderList() {
+    myList.innerHTML = '';
+    for (let i = 0; i < arr.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = arr[i];
+        let edit = document.createElement('button');
+        edit.textContent = 'Edit';
+        edit.addEventListener('click', function () {
+            let newText =prompt(" ");
+            if (newText !== null) {
+                arr[i] = newText;
+                renderList();
+            }
+        });
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', function () {
+            arr.splice(i, 1);
+            renderList();
+        });
+        li.appendChild(deleteButton);
+        li.appendChild(edit);
+        myList.appendChild(li);
+    }
+}
 
 
 
